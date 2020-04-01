4f56db7fd44fddf960244a7f5dbaced71854d85b
Sumit Behal <40502533+sumitbehal@users.noreply.github.com>
2020-04-01 20:50:40 +0530

    Update trump.py

--- trump.py
+++ trump.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/file/d/13EI0udX_Nsvxc6qh7xIQ59bAVq3MQ2sw/view'
+export_file_url = 'https://drive.google.com/uc?export=download&id=13EI0udX_Nsvxc6qh7xIQ59bAVq3MQ2sw'
 export_file_name = 'stage1.pth'
 
 classes = ['trump', 'modi']
